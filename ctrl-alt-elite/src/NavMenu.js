be2359c617e840039ef4ba6f2893d4abea59bb96
James Taylor <jt185269@ncr.com>
2022-07-27 12:30:29 +0100

    added expenses page

--- ctrl-alt-elite/src/NavMenu.js
+++ ctrl-alt-elite/src/NavMenu.js
@@ -2,21 +2,53 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from 'react';
+import {Expenses} from './Expenses'
+
+async function fetchData() {
+
+  let options= {
+
+    method: 'GET',
+
+    headers: { 'Content-Type': 'application/json' },
+
+  }
+
+  const res = await fetch('http://localhost:5000/api/datafetch', options)
+
+  return res.json();
+
+}
 
 export const NavMenu = () => {
+  const [page, setPage] = useState(null);
+  const data = fetchData().then( (res) =>
+    console.log(res)
+  );
+  
+  const onPageChange = (e) => {
+    console.log(e)
+    setPage(e)
+  }
+
   return (
+    <>
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link>Expenses</Nav.Link>
-            <Nav.Link>Income</Nav.Link>
-            <Nav.Link>Summary</Nav.Link>
-            <Nav.Link>Schedule</Nav.Link>
+            <Nav.Link onClick={() => onPageChange("expenses")}>Expenses</Nav.Link>
+            <Nav.Link onClick={() => onPageChange("income")}>Income</Nav.Link>
+            <Nav.Link onClick={() => onPageChange("summary")}>Summary</Nav.Link>
+            <Nav.Link onClick={() => onPageChange("schedule")}>Schedule</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    {page === "expenses" && <Expenses data={data}/>}
+    </>
+
   );
 }
